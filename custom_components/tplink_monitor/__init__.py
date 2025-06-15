621c03d749b25099227f263897ed3250bffff643
Michal Kubicki <michal.kubicki@me.com>
2025-06-16 01:37:22 +0200

    Update __init__.py
    As per recent info: https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/
    
    Calling hass.config_entries.async_forward_entry_setup is deprecated and will be removed in Home Assistant 2025.6. Instead, await hass.config_entries.async_forward_entry_setups

--- custom_components/tplink_monitor/__init__.py
+++ custom_components/tplink_monitor/__init__.py
@@ -13,7 +13,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
     # Reload if needed
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    hass.async_create_task(hass.config_entries.async_forward_entry_setups(entry, "sensor"))
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
